38898f564197ca44f93dd6111cd59e33bcf55c8b
[xiaowenjie] <[xiaowenjie@haoche51.com]>
2016-07-06 11:07:01 +0800

    22

--- i.js
+++ i.js
@@ -12,25 +12,36 @@ var opts = {
 
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(data) {
-		data = JSON.parse(data);
-		var q = querystring.stringify({'q': data.msg, 'uid': ws.uid});
-		opts.path = '/reply?'+q;
-		var req = http.request(opts, function(res) {
-			res.setEncoding('utf8');  
-			res.on('data', function(chunk) {
-				console.log(chunk);
-				chunk = JSON.parse(chunk);
-				if (chunk.data) {
-					var data = {
-						msg: chunk.data
-					};
-					ws.send(JSON.stringify(data));
-				}
-			});
-		});
-		req.end();
+		if (ws.status == null) {
+			ws.status = setTimeout(function() {
+				wss.reply(ws, data);
+				ws.status = null;
+			}, 2000);
+		}
     });
-	
+	ws.on('close', function() {
+        clearTimeout(ws.status);
+    });
 	ws.uid = uuid.v4();
+	ws.status = null;
 });
 
+wss.reply = function reply(ws, data) {
+	data = JSON.parse(data);
+	var q = querystring.stringify({'q': data.msg, 'uid': ws.uid});
+	opts.path = '/reply?'+q;
+	var req = http.request(opts, function(res) {
+		res.setEncoding('utf8');  
+		res.on('data', function(chunk) {
+			//console.log(chunk);
+			chunk = JSON.parse(chunk);
+			if (chunk.data) {
+				var data = {
+					msg: chunk.data
+				};
+				ws.send(JSON.stringify(data));
+			}
+		});
+	});
+	req.end();
+};
